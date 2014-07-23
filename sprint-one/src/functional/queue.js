0e7c29bb847773375c30089c8273e9e3d0e60131
Jon Sadka <jonsadka@gmail.com>
2014-07-23 15:09:46 -0700

    completed functional stack and progress on queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,42 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var sizeE = 0;
+  var sizeD = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[sizeE] = value;
+    sizeE++;
   };
 
   someInstance.dequeue = function(){
+    if ( sizeE !== sizeD){
+      var result = storage[sizeE-sizeD];
+      delete storage[sizeE-sizeD];
+      sizeD++;
+      return result;
+    }
+
   };
 
   someInstance.size = function(){
+    return sizeE-sizeD;
   };
 
   return someInstance;
 };
+
+// []      sizeE 0  sizeD 0
+// [a]     sizeE 1  sizeD 0
+// [a,b]   sizeE 2  sizeD 0
+// [a,b,c] sizeE 3  sizeD 0
+//   [b,c] sizeE 3  sizeD 1
+//     [c] sizeE 3  sizeD 2
+//      [] sizeE 3  sizeD 3
+
+// []  fin sizeE 0  sizeD 0
+// [a] fin sizeE 1  sizeD 0
+//  [] fin sizeE 1  sizeD 1
+// [a] fin sizeE 2  sizeD 1
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function(){
+    count && count--
+    var poppedVal = storage[count];
+    delete storage[count];
+    return poppedVal;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
