859c4a36c54c8872760d113fb5314acdac227c4a
Jon Sadka <jonsadka@gmail.com>
2014-10-18 09:16:59 -0700

    implement linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newLink = makeNode(value);
+    if (list.head === null) list.head = newLink;
+    if (list.tail !== null) list.tail.next = newLink;
+    list.tail = newLink;
   };
 
   list.removeHead = function(){
+    if ( list.head !== null ){
+      var headValue = list.head.value;
+      if (list.head === list.tail){
+        list.tail = null;
+        list.head = null;
+      } else {
+        list.head = list.head.next;
+      }
+      return headValue;
+    }
   };
 
   list.contains = function(target){
+    if (list.head === null) return;
+    var currentNode = list.head;
+    if (currentNode.value === target) return true;
+    while (currentNode.next !== null ){
+      currentNode = currentNode.next;
+      if (currentNode.value === target) return true;
+    }
+    return false;
   };
 
   return list;
