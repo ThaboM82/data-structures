dec1ebce7ab7aa767802771d782773ce9e897084
Jon Sadka <jonsadka@gmail.com>
2014-10-19 17:51:02 -0700

    progress on has resize

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,12 +1,19 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._entries = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
+console.log("Entered",this._entries, this._limit)
   var i = getIndexBelowMaxForKey(k, this._limit);
   if (!this._storage[i]) this._storage[i] = [];
   this._storage[i].push([k,v]);
+  this._entries++;
+  if ( this._entries > 0.75 * this._limit ){
+    this._limit *= 2;
+    this._storage = changeHash(this._limit, this._storage);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -22,17 +29,41 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+console.log("Removed",this._entries, this._limit, i, this._storage)
   if (this._storage[i]){
     var bucket = this._storage[i];
     for (var j = 0; j < bucket.length; j++){
-      if (bucket[j][0] === k) var value = bucket[j][1];
-      this._storage[i] = this._storage[i].splice(0,j) + this._storage[i].splice(j+1);
-      return value;
+      if (bucket[j][0] === k){
+        var value = bucket[j][1];
+        this._storage[i] = this._storage[i].splice(0,j) + this._storage[i].splice(j+1);
+        this._entries--;
+        if ( this._entries <= 0.5 * this._limit ){
+          this._limit /= 2;
+          this._storage = changeHash(this._limit, this._storage);
+        }
+        return value;
+      }
     }
   }  
 };
 
-
+function changeHash(newLimit, oldStorage){
+  newStorage = makeLimitedArray(newLimit);
+  var bucketHashes = Object.keys(oldStorage);
+  bucketHashes.forEach(function(bucketHash){
+    var bucket = oldStorage[bucketHash];
+    if ( Array.isArray(bucket) ){
+      bucket.forEach(function(keyvalue){
+        var key = keyvalue[0];
+        var value = keyvalue[1];
+        var i = getIndexBelowMaxForKey(key, newLimit);
+        if ( !newStorage[i] ) newStorage[i] = [];
+        newStorage[i].push([key,value]);
+      });
+    }
+  })  
+  return newStorage;
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
