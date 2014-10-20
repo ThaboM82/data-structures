0479533ad4e7f90b37858ce243ab183d6311ea50
Jon Sadka <jonsadka@gmail.com>
2014-10-19 20:00:00 -0700

    implement binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,31 @@
-var makeBinarySearchTree = function(value){
+var makeBinarySearchTree = function(val){
+  var tree = makeNode(val);
+  function makeNode(value){
+    return {
+      value:value,
+      left:null,
+      right:null,
+      insert: function(val){
+        if ( val <= this.value ){
+          this.left ? this.left.insert(val) : this.left = makeNode(val);
+        } else {
+          this.right ? this.right.insert(val) : this.right = makeNode(val);
+        }
+      },
+      contains: function(target){
+        if (this.value === target) return true;
+        if (target <= this.value && this.left) return this.left.contains(target);
+        if (target > this.value && this.right) return this.right.contains(target);
+        return false;
+      },
+      depthFirstLog: function(callback){
+        this.value = callback(this.value);
+        if (this.left) this.left.depthFirstLog(callback);
+        if (this.right) this.right.depthFirstLog(callback);
+      }
+    };
+  }
+  return tree;
 };
 
 
